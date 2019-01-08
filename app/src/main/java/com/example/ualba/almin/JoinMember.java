f115312625fe9d9eb0bde300c38840e993ad0f72
hjshin <shj950505@naver.com>
2019-01-08 18:32:21 +0900

    2019-01-08 신현주  JoinMemberFirst 화면 추가

--- app/src/main/java/com/example/ualba/almin/JoinMember.java
+++ app/src/main/java/com/example/ualba/almin/JoinMember.java
@@ -1,7 +1,10 @@
 package com.example.ualba.almin;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class JoinMember extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class JoinMember extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.join_member);
+
+        Button joinbtn = (Button)findViewById(R.id.nextPage);
+
+        joinbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(JoinMember.this, JoinMemberFirst.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/ualba/almin/JoinMemberFirst.java
+++ app/src/main/java/com/example/ualba/almin/JoinMemberFirst.java
@@ -1,13 +1,27 @@
 package com.example.ualba.almin;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class JoinMemberFirst extends AppCompatActivity {
+    private static final int SEARCH_ADDRESS_ACTIVITY = 10000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.join_member_first);
+
+        Button addressBtn = (Button)findViewById(R.id.addressbtn);
+        addressBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                startActivityForResult(intent, SEARCH_ADDRESS_ACTIVITY);
+            }
+        });
+
     }
 }
