34a4686f324868eecedb873cf5ce98a56ffbc431
hjshin <shj950505@naver.com>
2019-01-08 15:51:44 +0900

    2019-01-08 신현주 JoinMember 화면 추가

---
+++ app/src/main/java/com/example/ualba/almin/JoinMember.java
@@ -0,0 +1,13 @@
+package com.example.ualba.almin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class JoinMember extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.join_member);
+    }
+}
---
+++ app/src/main/java/com/example/ualba/almin/JoinMemberFirst.java
@@ -0,0 +1,13 @@
+package com.example.ualba.almin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class JoinMemberFirst extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.join_member_first);
+    }
+}
