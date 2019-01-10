bb2cb14f3a38b6ffe2a704476f8ed6882a18a3a3
hjshin <shj950505@naver.com>
2019-01-10 14:47:15 +0900

    2019-01-10 신현주 다음주소 검색

--- app/src/main/java/com/example/ualba/almin/JoinMemberFirst.java
+++ app/src/main/java/com/example/ualba/almin/JoinMemberFirst.java
@@ -3,25 +3,63 @@ package com.example.ualba.almin;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class JoinMemberFirst extends AppCompatActivity {
     private static final int SEARCH_ADDRESS_ACTIVITY = 10000;
-
+    private EditText et_address;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.join_member_first);
 
+
+        et_address = (EditText)findViewById(R.id.et_address);
+
         Button addressBtn = (Button)findViewById(R.id.addressbtn);
+
         addressBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent();
+                Intent intent = new Intent(JoinMemberFirst.this, JoinMemberWebview.class);
                 startActivityForResult(intent, SEARCH_ADDRESS_ACTIVITY);
             }
         });
 
+        Intent intent = getIntent();
+        String addr  =  intent.getStringExtra("addr");
+
+        if("" != addr){
+            Log.d("현주", "ifififififi");
+            et_address.setText(addr);
+        }
+
     }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent intent){
+
+        super.onActivityResult(requestCode, resultCode, intent);
+
+        switch(requestCode){
+
+            case SEARCH_ADDRESS_ACTIVITY:
+
+                if(resultCode == RESULT_OK){
+
+                    String data = intent.getExtras().getString("data");
+                    if (data != null)
+                        et_address.setText(data);
+
+                }
+                break;
+
+        }
+
+    }
+
+
 }
--- app/src/main/java/com/example/ualba/almin/JoinMemberWebview.java
+++ app/src/main/java/com/example/ualba/almin/JoinMemberWebview.java
@@ -1,13 +1,68 @@
 package com.example.ualba.almin;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class JoinMemberWebview extends AppCompatActivity {
+    private WebView webView;
+    private TextView result;
+    private Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.join_member_webview);
+
+        result = (TextView)findViewById( R.id.result);
+        init_WebView();
+        handler = new Handler();
+    }
+
+    public void init_WebView(){
+        webView = (WebView)findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+        webView.addJavascriptInterface(new AndroidBridge(), "TestApp");
+        // web client 를 chrome 으로 설정
+        webView.setWebChromeClient(new WebChromeClient());
+        // webview url load
+//        webView.loadUrl("file:///android_asset/search_address.html");
+        webView.loadUrl("http://codeman77.ivyro.net/getAddress.php");
+
+    }
+
+    private class AndroidBridge {
+        @JavascriptInterface
+        public void setAddress(final String arg1, final String arg2, final String arg3) {
+            handler.post(new Runnable() {
+                @Override
+                public void run() {
+                    Toast.makeText(JoinMemberWebview.this, arg1+arg2+arg3, Toast.LENGTH_SHORT).show();
+//                    Toast.makeText(WebViewActivity.this, arg1, Toast.LENGTH_SHORT).show();
+//                    Toast.makeText(WebViewActivity.this, arg1, Toast.LENGTH_SHORT).show();
+
+                    result.setText(String.format("(%s) %s %s", arg1, arg2, arg3));
+                    // WebView를 초기화 하지않으면 재사용할 수 없음
+                    init_WebView();
+                    //onPause();
+
+                    Intent intent=new Intent(JoinMemberWebview.this, JoinMemberFirst.class);
+
+                    String addr = arg1+arg2+arg3;
+//                    Toast.makeText(WebViewActivity.this, addr, Toast.LENGTH_SHORT).show();
+                    intent.putExtra("addr", addr);
+                    startActivity(intent);
+                    finish();
+                }
+            });
+
+        }
     }
 }
