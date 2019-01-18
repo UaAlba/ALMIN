2987416be0ae7cb1d9086dcbfec1445ec9478b30
Ban <ucdc9911@naver.com>
2019-01-18 17:45:30 +0900

    second commit

--- app/src/main/java/com/example/ualba/almin/JoinMember.java
+++ app/src/main/java/com/example/ualba/almin/JoinMember.java
@@ -3,14 +3,19 @@ package com.example.ualba.almin;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import com.amazonaws.mobile.client.AWSMobileClient;
+import com.amazonaws.mobile.client.AWSStartupHandler;
+import com.amazonaws.mobile.client.AWSStartupResult;
 
 public class JoinMember extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.join_member);
 
         Button joinbtn = (Button)findViewById(R.id.nextPage);
--- app/src/main/java/com/example/ualba/almin/MainActivity.java
+++ app/src/main/java/com/example/ualba/almin/MainActivity.java
@@ -1,13 +1,25 @@
 package com.example.ualba.almin;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+
+        import com.amazonaws.mobile.config.AWSConfiguration;
+        import com.amazonaws.mobileconnectors.appsync.AWSAppSyncClient;
 
 public class MainActivity extends AppCompatActivity {
+
+    private AWSAppSyncClient mAWSAppSyncClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mAWSAppSyncClient = AWSAppSyncClient.builder()
+                .context(getApplicationContext())
+                .awsConfiguration(new AWSConfiguration(getApplicationContext()))
+                .build();
+
         setContentView(R.layout.activity_main);
     }
 }
